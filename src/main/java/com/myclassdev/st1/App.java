72a923e03bd4858f176cdbad47dbd32ae74443c6
ashwin1-dev <root@localhost.localdomain>
2024-04-15 15:55:27 +0000

    add to local repo

---
+++ src/main/java/com/myclassdev/st1/App.java
@@ -0,0 +1,17 @@
+package com.myclassdev.st1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Samsung" );
+        System.out.println( "Nokia" );
+        System.out.println( "OnePlus" );
+        System.out.println( "Mi" );
+    }
+}
